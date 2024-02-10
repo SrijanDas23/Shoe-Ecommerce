845947a0cda7fc8438234d32751987818f39896b
SrijanDas23 <157733127+SrijanDas23@users.noreply.github.com>
2024-02-10 21:30:31 +0530

    navbar
    made a functional navbar(kinda)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-function App() {
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetails/ItemDetails";
+import Checkout from "./scenes/checkout/checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+import Navbar from "./scenes/global/Navbar";
+import CartMenu from "./scenes/global/CartMenu";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
+function App() {
   return (
-    <>
-      
-    </>
-  )
+    <div className="app">
+      <BrowserRouter>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="item/:itemId" element={<ItemDetails />}></Route>
+          <Route path="checkout" element={<Checkout />}></Route>
+          <Route path="checkout/success" element={<Confirmation />}></Route>
+        </Routes>
+        <CartMenu />
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { theme } from "./theme.js";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./state";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const store = configureStore({
+  reducer: { cart: cartReducer },
+});
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
+);
